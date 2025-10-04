64a248240d504ba57f02cfb5dc8ceffa5c661c9c
Maxime <maxime.estrade11@icloud.com>
2025-10-04 15:08:22 +0200

    update poetic timing and semantic map improvements

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { injectSpeedInsights } from '@vercel/speed-insights';
-import { injectSpeedInsights } from '@vercel/speed-insights';
 import { inject } from '@vercel/analytics';
 import App from "./App";
 import "./index.css";

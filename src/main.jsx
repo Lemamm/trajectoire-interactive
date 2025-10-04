d98ef7fa672fafc3ca6d543148456e926184bc94
Maxime <maxime.estrade11@icloud.com>
2025-10-04 14:51:30 +0200

    update poetic timing and semantic map improvements

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { injectSpeedInsights } from '@vercel/speed-insights';
+import { injectSpeedInsights } from '@vercel/speed-insights';
+import { inject } from '@vercel/analytics';
 import App from "./App";
 import "./index.css";
 injectSpeedInsights();
+inject();
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
